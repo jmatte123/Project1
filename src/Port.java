f568b79ae51e2c5ddc1b52ab4d95e132ea0ab5fa
jmatte123 <joseph1matteson@gmail.com>
2017-10-13 21:49:09 -0500

    Port javadoced

--- src/Port.java
+++ src/Port.java
@@ -2,95 +2,92 @@ import java.util.*;
 /**
  * @author Joe Matteson
  * @author Edgar Centeno
+ *
+ * this is the Port class which represents a port for ships and thier cargo.
  */
 public class Port
 {
+    private static int nextID = 0;
     private String name = ""; //Order by name
     private ArrayList<Cargo> local = new ArrayList<Cargo>(); //Local Cargo
     private ArrayList<Cargo> outbound = new ArrayList<Cargo>(); //Outbound Cargo
     private Random gen = new Random();
     private int myID; //equals()
-    private static int nextID = 0;
-     
-    public boolean equals(Port other)
-     {
-          if (getID() == other.getID())
-               return true;
-          return false;
-     }
-     
-     public int compareTo(Port other)
-     {
-          return getName().compareTo(other.getName()); 
-     }
-     
+
+    /**
+     * Default Port constructor.  Creates a new port with a new id.
+     */
+    public Port()
+    {
+        setID();
+    }
+
+    /**
+     * This is a constructor  that takes in a name for the new port.
+     * @param inName name of the port.
+     */
     public Port(String inName)
     {
          setName(inName);
          setID();
     }
-    
-    public Port()
-    {
-         setID();
-    }
-    
+
+    /**
+     * increment the id to get a new id for a new port.
+     */
     private void setID()
     {
          myID = nextID++;
     }
-    
+
+    /**
+     * get the id of the port.
+     * @return the ports id
+     */
     public int getID()
     {
          return myID;
     }
-    
-    public String toString()
-    {
-         String output = "";
-         output = "This is " +getName()+ ":\n";
-         output = output + "\tPort Number: " + getID() + "\n*****LOCAL CARGO*********\n";
-         for (Cargo unit : local)
-         {
-              output = output + unit;
-         }
-         output = output + "*****END LOCAL CARGO*********\n\tTotal Tonnage of Local Cargo: " + getLocalTonnage() + "\n*****OUTBOUND CARGO*********\n";
-         for (Cargo unit : outbound)
-         {
-              output = output + unit;
-         }
-         output = output + "*****END OUTBOUND CARGO*********\n\tTotal Tonnage of Outbound Cargo: " + getOutboundTonnage() + "\n";
-         return output;
-    }
-    
+
+    /**
+     * get the local cargo at the port.
+     * @return the cargo.
+     */
     public ArrayList<Cargo> getLocal()
     {
-         return local; 
+         return local;
     }
 
+    /**
+     * get the cargo that is being shipped oit somewhere else.
+     * @return the cargo that is being shipped.
+     */
     public ArrayList<Cargo> getOutbound()
     {
-         return outbound; 
+         return outbound;
     }
 
+    /**
+     * get the name of the port.
+     * @return the name of the port.
+     */
     public String getName()
     {
          return name;
     }
 
-    /*
-      This is stars inside
-      it does multiple lines
+    /**
+     * set the name of the port.
+     * @param inName the new name of the port.
      */
-
-    public void addOutbound(Cargo inCargo)
-    {
-         if (inCargo.getTonnage() >= 0)
-         {
-              outbound.add(inCargo);
-         }
+    public void setName(String inName) {
+         name = inName;
     }
-    
+
+    /**
+     * get the local tonnage.
+     * @return the total amount of tonnage in the local stock.
+     */
     public double getLocalTonnage()
     {
          double total = 0.0;
@@ -100,7 +97,11 @@ public class Port
          }
          return total;
     }
-    
+
+    /**
+     * get the amount of tonnage that the outbound has.
+     * @return the total amount of tonnage.
+     */
     public double getOutboundTonnage()
     {
          double total = 0.0;
@@ -111,11 +112,22 @@ public class Port
          return total;
     }
 
-    public void setName(String inName)
+    /**
+     * adds cargo to the outbound cargo pile.
+     * @param inCargo cargo to put on the outbound.
+     */
+    public void addOutbound(Cargo inCargo)
     {
-         name = inName;
+        if (inCargo.getTonnage() >= 0)
+        {
+            outbound.add(inCargo);
+        }
     }
-    
+
+    /**
+     * load cargo onto the ship.
+     * @param targetShip the ship that will take the cargo.
+     */
     public void load(Ship targetShip)
     {
          for (int x = outbound.size() - 1; x >=0 ; x--)
@@ -127,6 +139,10 @@ public class Port
          }
     }
 
+    /**
+     * unloads certain cargo from the ship.
+     * @param targetShip the ship that will be unloaded.
+     */
     public void unload(Ship targetShip)
     {
          ArrayList<Cargo> unloaded = targetShip.unload(getName());
@@ -141,10 +157,13 @@ public class Port
                    outbound.add(unit);
               }
          }
-         
+
     }
 
-    
+    /**
+     * unloads all the cargo from the ship and sorts it out given the cargo's destination.
+     * @param targetShip the ship that will be unloaded.
+     */
     public void unloadAll(Ship targetShip)
     {
          ArrayList<Cargo> unloaded = targetShip.unloadAll();
@@ -159,6 +178,51 @@ public class Port
                    outbound.add(unit);
               }
          }
-         
+
+    }
+
+    /**
+     * returns true or false depending upon if two ports have the same id.
+     * @param other the other port for testing if it is equal.
+     * @return true if it is the same otherwise false.
+     */
+    public boolean equals(Port other)
+    {
+        if (getID() == other.getID())
+            return true;
+        return false;
+    }
+
+    /**
+     * compares the names of two ports.
+     * @param other the other port to compare it to
+     * @return negative if the string is smaller than the other, positive if the string is bigger,
+     * and 0 if the string is the same size.
+     */
+    public int compareTo(Port other)
+    {
+        return getName().compareTo(other.getName());
+    }
+
+    /**
+     * string representation of the class.
+     * @return the output format.
+     */
+    public String toString()
+    {
+        String output = "";
+        output = "This is " +getName()+ ":\n";
+        output = output + "\tPort Number: " + getID() + "\n*****LOCAL CARGO*********\n";
+        for (Cargo unit : local)
+        {
+            output = output + unit;
+        }
+        output = output + "*****END LOCAL CARGO*********\n\tTotal Tonnage of Local Cargo: " + getLocalTonnage() + "\n*****OUTBOUND CARGO*********\n";
+        for (Cargo unit : outbound)
+        {
+            output = output + unit;
+        }
+        output = output + "*****END OUTBOUND CARGO*********\n\tTotal Tonnage of Outbound Cargo: " + getOutboundTonnage() + "\n";
+        return output;
     }
 }
